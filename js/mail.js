d635e6cf976b476d6e08ddd2267ee19623299d1c
Tom Cuypers <tom.cuypers@kiswe.com>
2018-10-15 10:31:39 +0200

    Update mail

--- js/mail.js
+++ js/mail.js
@@ -5,7 +5,7 @@ function sendMail(message, email, name) {
     var params = 'message=' + message + '&emailaddress=' + email;
     $.ajax({
         dataType: 'json',
-        url: 'https://script.google.com/macros/s/1gXcsSu-KJ5BS18VBwQWSJXTE5UehEYSEUluvsc3sXEtlaWu072XN9mXT/exec?' + params,
+        url: 'https://script.google.com/macros/s/AKfycbzCXrbdhxOeCQrP7pUZg1FPd2vdnxSSG1bOcHdi9VnzH_f_DaEe/exec?' + params,
         success: function () {
             formSuccess();
         },
